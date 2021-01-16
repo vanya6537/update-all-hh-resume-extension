d0da8f96b107a3a3708e9c1ed6f830f02a1b34ae
Netsl <vanya6537@gmail.com>
2021-01-16 19:17:11 +0300

    Version with execution every four hours

--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
 function injectTheScript() {
-    try {
-        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            // query the active tab, which will be only one tab
-            //and inject the script in it
-            chrome.tabs.executeScript(tabs[0].id, {file: "content_script.js"});
-        });
-    }catch (err){
-        console.error(err);
-    }
+  try {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      // Query the active tab, which will be only one tab
+      // And inject the script in it
+      chrome.tabs.executeScript(tabs[0].id, { file: "content_script.js" });
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-document.getElementById('cta').addEventListener('click', injectTheScript);
+document.getElementById("cta").addEventListener("click", injectTheScript);
