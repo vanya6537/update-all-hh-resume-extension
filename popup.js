ba7c124371f4324ef53027c903dc891710cdb8fa
Netsl <vanya6537@gmail.com>
2021-03-10 12:56:32 +0400

    refactor: injectTheScript was removed from popup.js

--- popup.js
+++ popup.js
@@ -1,13 +1,5 @@
-function injectTheScript() {
-  try {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      // Query the active tab, which will be only one tab
-      // And inject the script in it
-      chrome.tabs.executeScript(tabs[0].id, { file: "content_script.js" });
-    });
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-document.getElementById("cta").addEventListener("click", injectTheScript);
+document
+  .getElementById("cta")
+  .addEventListener("click", () =>
+    chrome.tabs.executeScript({ file: "content_script.js" })
+  );
